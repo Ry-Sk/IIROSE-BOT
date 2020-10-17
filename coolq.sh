0819672edfd6971b6fd0323aa0f5f6218f0fb6bc
logos <1102280066@qq.com>
2020-10-17 13:04:02 +0800

    Delete coolq.sh

--- coolq.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-docker pull richardchien/cqhttp:latest
-mkdir coolq
-docker run -ti --rm --name cqhttp-test \
-           -v $(pwd)/coolq:/home/user/coolq \
-           -p 9000:9000 \
-           -p 5700:5700 \
-           -e COOLQ_ACCOUNT=2835965744 \
-           richardchien/cqhttp:latest
